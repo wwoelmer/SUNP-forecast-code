6f4fed832d84fc661cc9b737d02baec93fe35b0e
Whitney Woelmer <wwoelmer@vt.edu>
2024-06-14 17:01:26 +1200

    updating reliability plots for all vars, depths, horizons

--- R/retired_scripts/reliability_diagrams.R
+++ R/retired_scripts/reliability_diagrams.R
@@ -7,61 +7,39 @@ lake_directory <- here::here()
 
 # some subsetting variables
 vars <- c('temperature', 'oxygen')
-depths <- c(0.1, 1.0, 10.0, 30.0)
+depths <- c(1.0, 10.0)
 horizons <- c(1:35)
-folders <- c('all_UC', 'initial_condition', 'observation', 'parameter', 'process', 'weather')
+sim_name <- 'SUNP_fcasts_temp_DO' 
 #folders <- c('initial_condition')
 
 ########################################################################
 # read in the scores and calculate variance
-score_dir <- arrow::SubTreeFileSystem$create(file.path(lake_directory,"scores/sunp/UC_analysis_2021", folders[1]))
+score_dir <- arrow::SubTreeFileSystem$create(file.path(lake_directory,"scores/sunp", sim_name))
 
 sc <- arrow::open_dataset(score_dir) |> 
   filter(variable %in% vars,
          depth %in% depths) %>% 
   collect() 
 
-for(i in 1:length(folders)){
-  score_dir <- arrow::SubTreeFileSystem$create(file.path(lake_directory,"scores/sunp/UC_analysis_2021", folders[i]))
-  
-  temp <- arrow::open_dataset(score_dir) |> 
-    filter(variable %in% vars,
-           depth %in% depths) %>% 
-    collect() 
-  
-  sc <- rbind(sc, temp)
-  
-}
 
-# now read in 2022 data
-for(i in 1:length(folders)){
-  score_dir <- arrow::SubTreeFileSystem$create(file.path(lake_directory,"scores/sunp", folders[i]))
-  
-  temp <- arrow::open_dataset(score_dir) |> 
-    filter(variable %in% vars,
-           depth %in% depths) %>% 
-    collect() 
-  
-  sc <- rbind(sc, temp)
-  
-}
-
-# make vector of dates when obs are available (before buoy is taken into harbor)
-buoy_dates <- c(seq.Date(as.Date('2021-08-04'), as.Date('2021-10-19'), by = 'day'),
-                seq.Date(as.Date('2022-06-01'), as.Date('2022-10-17'), by = 'day'))
+# vector of dates when obs are available (before buoy is taken into harbor)
+##### subset to the same time frame for each year for equal comparison
+buoy_dates <- c(seq.Date(as.Date('2021-08-04'), as.Date('2021-10-17'), by = 'day'),
+                seq.Date(as.Date('2022-08-04'), as.Date('2022-10-17'), by = 'day'))
 
 sc <- sc %>% 
   mutate(doy = yday(datetime),
          year = year(datetime)) %>% 
   select(-c(family, site_id)) %>% 
   filter(horizon > 0,
-         as.Date(datetime) %in% buoy_dates) %>% 
+         as.Date(reference_datetime) %in% buoy_dates) %>% 
   select(model_id, reference_datetime, datetime, horizon, depth, variable, everything()) 
 
-# convert oxy crps to mg/L
+# convert oxy crps and obs to mg/L
 sc <- sc %>% 
   mutate(crps = ifelse(variable=='temperature', crps, (crps*32/1000)),
-         observation_mgL_C = ifelse(variable=='temperature', observation, (observation*32/1000))) 
+         mean = ifelse(variable=='temperature', mean, (mean*32/1000)),
+         observation = ifelse(variable=='temperature', observation, (observation*32/1000)))
 
 
 ##########################################################################
@@ -70,8 +48,8 @@ rd <- sc %>%
   select(reference_datetime, datetime, depth, model_id, variable, horizon,
          observation, mean, sd, doy, year) %>% 
   filter(!is.na(observation),
-         variable=="oxygen", # all grouping variables
-         horizon==35,   
+         variable=="temperature", # all grouping variables
+         horizon==7,   
          depth==10)
 
 
@@ -79,21 +57,51 @@ dat <- data.frame(bin = seq(10, 90, by = 10),
                   obs_freq = NA,
                   low = c(0.45, 0.40, 0.35, 0.3, 0.25, 0.20, 0.15, 0.10, 0.05),
                   high = c(0.55, 0.60, 0.65, 0.7, 0.75, 0.80, 0.85, 0.9, 0.95),
-                  UC = 'all_UC')
+                  UC = 'all_UC_fsed_deep_DA')
+df <- expand.grid(variable = c('temperature', 'oxygen'),
+                depth = c(1, 10),
+                horizon = c(1, 7, 35),
+                bin = seq(10, 90, by = 10),
+                obs_freq = NA,
+                low = c(0.45, 0.40, 0.35, 0.3, 0.25, 0.20, 0.15, 0.10, 0.05),
+                high = c(0.55, 0.60, 0.65, 0.7, 0.75, 0.80, 0.85, 0.9, 0.95),
+                UC = 'all_UC_fsed_deep_DA')
+df
+
+folders <- "all_UC_fsed_deep_DA"
 
 
-for(i in 2:length(folders)){
-  temp <- data.frame(bin = seq(10, 90, by = 10),
-                    obs_freq = NA,
-                    low = c(0.45, 0.40, 0.35, 0.3, 0.25, 0.20, 0.15, 0.10, 0.05),
-                    high = c(0.55, 0.60, 0.65, 0.7, 0.75, 0.80, 0.85, 0.9, 0.95),
-                    UC = folders[i])
-  dat <- rbind(dat, temp)
-}
+
+
+out <- plyr::ddply(df, c("variable", "depth", "horizon", "bin"), \(x){
+  print(head(x))
+  temp <- rd
+  vec <- c()
+
+    for(i in 1:nrow(temp)){
+      # subset to forecasts within each break range of the distribution
+      prob_above <- qnorm(x$high[j], temp$mean[i], temp$sd[i])
+      prob_below <- qnorm(x$low[j], temp$mean[i], temp$sd[i])
+     
+      if(temp$observation[i] < prob_above & temp$observation[i] > prob_below){
+        # 1 if obs in in CI, 0 if not
+        vec <- c(vec, 1)
+      }else{
+        vec <- c(vec, 0)
+      }
+      
+      
+    }
+    x$obs_freq[j] <- sum(vec)/nrow(temp)*100
+    vec <- c()
+    
+    return(x)
+
+
+})
 
 
 vec <- c()
-
 for(k in 1:length(folders)){
   temp <- rd[rd$model_id==folders[k],]
   temp_dat <- dat[dat$UC==folders[k],]
@@ -123,7 +131,7 @@ for(k in 1:length(folders)){
 
 
 ggplot(dat, aes(x = bin, y = bin, color = UC)) +
+  geom_line(aes(y = obs_freq)) +  
   geom_point(color = 'black') +
-  geom_line(aes(y = obs_freq)) +
-  ggtitle('oxygen 10m horizon = 35')
+  ggtitle('temperature 10m horizon = 1')
 
